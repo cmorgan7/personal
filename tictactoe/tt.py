7d71e97d4f05337dedcf53b1593307cc43b87e8e
Cole <cmorgan9er@gmail.com>
2020-08-13 23:52:23 -0400

    Update tt.py

--- tictactoe/tt.py
+++ tictactoe/tt.py
@@ -1,14 +1,16 @@
-board = [['7','8','9'],['4','5','6'],['1','2','3']]
-playing_board = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
+board = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']]
+playing_board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
 current_letter = []
 count = 0
 
+
 def display(lst):
-    print(f'\n {lst[0][0]} | {lst[0][1]} | {lst[0][2]} ')
+    print(f'\n {lst[2][0]} | {lst[2][1]} | {lst[2][2]} ')
     print('-----------')
     print(f' {lst[1][0]} | {lst[1][1]} | {lst[1][2]} ')
     print('-----------')
-    print(f' {lst[2][0]} | {lst[2][1]} | {lst[2][2]} \n')
+    print(f' {lst[0][0]} | {lst[0][1]} | {lst[0][2]} \n')
+
 
 def select_letter():
     global current_letter
@@ -23,33 +25,30 @@ def select_letter():
         else:
             print("Invalid response\n")
 
+
 def player_choice():
     while True:
         choice = input("\nSelect a number on the board: ")
-        if not any(choice in x for x in board) or choice == '': #and choice not in board[1] and choice not in board[2]:
+        if not any(
+                choice in x for x in board) or choice == '':  # and choice not in board[1] and choice not in board[2]:
             print("Invalid response\n")
         else:
             break
     val = int(choice) - 1
-    if val//3 == 0:
-        row = 2
-    elif val//3 == 2:
-        row = 0
-    else:
-        row = 1
-    playing_board[row][val%3] = current_letter[count%2]
-    board[row][val%3] = ''
+    playing_board[val//3][val % 3] = current_letter[count % 2]
+    board[val//3][val % 3] = ''
+
 
 def gameover(letter):
     global count
     if (letter == playing_board[0][0] == playing_board[0][1] == playing_board[0][2]
-    or letter == playing_board[1][0] == playing_board[1][1] == playing_board[1][2]
-    or letter == playing_board[2][0] == playing_board[2][1] == playing_board[2][2]
-    or letter == playing_board[0][0] == playing_board[1][0] == playing_board[2][0]
-    or letter == playing_board[0][1] == playing_board[1][1] == playing_board[2][1]
-    or letter == playing_board[0][2] == playing_board[1][2] == playing_board[2][2]
-    or letter == playing_board[0][0] == playing_board[1][1] == playing_board[2][2]
-    or letter == playing_board[0][2] == playing_board[1][1] == playing_board[2][0]):
+            or letter == playing_board[1][0] == playing_board[1][1] == playing_board[1][2]
+            or letter == playing_board[2][0] == playing_board[2][1] == playing_board[2][2]
+            or letter == playing_board[0][0] == playing_board[1][0] == playing_board[2][0]
+            or letter == playing_board[0][1] == playing_board[1][1] == playing_board[2][1]
+            or letter == playing_board[0][2] == playing_board[1][2] == playing_board[2][2]
+            or letter == playing_board[0][0] == playing_board[1][1] == playing_board[2][2]
+            or letter == playing_board[0][2] == playing_board[1][1] == playing_board[2][0]):
         print(f'Winner: {letter}')
         return True
     elif count == 9:
@@ -60,8 +59,8 @@ def gameover(letter):
 
 def restart():
     global board, playing_board, count
-    board = [['7','8','9'],['4','5','6'],['1','2','3']]
-    playing_board = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
+    board = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']]
+    playing_board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
     count = 0
 
 
@@ -76,7 +75,7 @@ while True:
         continue
     select_letter()
     display(board)
-    while not gameover(current_letter[count%2-1]):
+    while not gameover(current_letter[count % 2 - 1]):
         player_choice()
         count += 1
         display(playing_board)
